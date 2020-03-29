6c43049853c1620c8e740bfaf9861bf66e01b25a
Andrés Troiano <61122404+andres-troiano@users.noreply.github.com>
2020-03-29 16:08:49 -0300

    Update modulo_redes.py

--- modulo_redes.py
+++ modulo_redes.py
@@ -11,7 +11,6 @@ def ldata(archive):
 # para hacer split de strings
 def split(txt, seps):
     default_sep = seps[0]
-
     for sep in seps[1:]:
         txt = txt.replace(sep, default_sep)
     return [i.strip() for i in txt.split(default_sep)]
